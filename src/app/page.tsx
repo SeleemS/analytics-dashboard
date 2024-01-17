0a82de92031f808da8241cfc577e21380c1d1ced
Seleem <sseleem1601@gmail.com>
2024-01-17 17:33:27 -0500

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Geography from '@/components/geography';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-10 py-5 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between px-10 py-5">
       <KeyStats />
       <div className="flex flex-row my-8 justify-center w-full gap-8">
       <Audience />
