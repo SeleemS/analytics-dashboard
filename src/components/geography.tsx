7c227fd62c9ecfa4b65d56c5761dfbad59c6863f
Seleem <sseleem1601@gmail.com>
2024-01-17 17:30:52 -0500

    x

--- src/components/geography.tsx
+++ src/components/geography.tsx
@@ -3,53 +3,98 @@ import { Card, Metric, Text, List, ListItem, ProgressBar, Grid } from "@tremor/r
 const byCountry = [
   {
     name: "Egypt",
-    share: 34,
+    share: 46,
     amount: "$ 11,715",
   },
   {
     name: "Saudi Arabia",
-    share: 24,
+    share: 28,
     amount: "$ 8,269",
   },
   {
     name: "UAE",
-    share: 11,
+    share: 22,
+    amount: "$ 3,790",
+  },
+  {
+    name: "Yemen",
+    share: 14,
+    amount: "$ 3,790",
+  },
+  {
+    name: "Morocco",
+    share: 7,
+    amount: "$ 3,790",
+  },
+  {
+    name: "Jordan",
+    share: 5,
     amount: "$ 3,790",
   },
 ];
 
 const byCity = [
   {
-    name: "Product C",
+    name: "Cairo",
     share: 89,
     amount: "$ 84,283",
   },
   {
-    name: "Product E",
+    name: "New Cairo",
     share: 79,
     amount: "$ 74,813",
   },
   {
-    name: "Product A",
+    name: "Giza",
+    share: 72,
+    amount: "$ 68,184",
+  },
+  {
+    name: "Abu Dhabi",
+    share: 89,
+    amount: "$ 84,283",
+  },
+  {
+    name: "Riyadh",
+    share: 79,
+    amount: "$ 74,813",
+  },
+  {
+    name: "Dubai",
     share: 72,
     amount: "$ 68,184",
   },
 ];
 
-const locationC = [
+const topVideos = [
   {
-    name: "Product A",
-    share: 45,
+    name: "Video 1",
+    share: 75,
     amount: "$ 27,955",
   },
   {
-    name: "Product D",
-    share: 35,
+    name: "Video 2",
+    share: 66,
     amount: "$ 21,743",
   },
   {
-    name: "Product C",
-    share: 75,
+    name: "Video 3",
+    share: 52,
+    amount: "$ 46,592",
+  },
+  {
+    name: "Video 4",
+    share: 50,
+    amount: "$ 27,955",
+  },
+  {
+    name: "Video 5",
+    share: 42,
+    amount: "$ 21,743",
+  },
+  {
+    name: "Video 6",
+    share: 31,
     amount: "$ 46,592",
   },
 ];
@@ -63,14 +108,18 @@ const categories = [
     title: "Views • By City",
     data: byCity,
   },
+  {
+    title: "Top Videos • Last 28 Days",
+    data: topVideos,
+  },
 ];
 
 export default function Geography() {
   return (
     <Grid numItemsSm={2} numItemsLg={3} className="gap-6 mb-6 justify-start">
       {categories.map((item) => (
-        <Card key={item.title} className="w-[30vw]">
-          <Text>{item.title}</Text>
+        <Card key={item.title} className="w-[30vw] shadow-xl" decoration="top" decorationColor="red">
+          <Text className="font-semibold">{item.title}</Text>
           <List className="mt-4">
             {item.data.map((product) => (
               <ListItem key={product.name}>
