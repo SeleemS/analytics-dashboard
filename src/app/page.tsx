dcd85bcc77ccd56628aa571b2bedeaaaa9880f22
Seleem <sseleem1601@gmail.com>
2024-01-16 19:59:33 -0500

    Update keystats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-row gap-10">
     <Card className="max-w-xs mx-auto">
-      <Text>Sales</Text>
+      <Text>Views</Text>
       <Metric>$ 71,465</Metric>
       <Flex className="mt-4">
         <Text>32% of annual target</Text>
@@ -13,6 +14,34 @@ export default function Home() {
       </Flex>
       <ProgressBar value={32} className="mt-2" />
     </Card>
+    <Card className="max-w-xs mx-auto">
+      <Text>Watch Hours</Text>
+      <Metric>$ 71,465</Metric>
+      <Flex className="mt-4">
+        <Text>32% of annual target</Text>
+        <Text>$ 225,000</Text>
+      </Flex>
+      <ProgressBar value={32} className="mt-2" />
+    </Card>
+    <Card className="max-w-xs mx-auto">
+      <Text>Subscribers</Text>
+      <Metric>$ 71,465</Metric>
+      <Flex className="mt-4">
+        <Text>32% of annual target</Text>
+        <Text>$ 225,000</Text>
+      </Flex>
+      <ProgressBar value={32} className="mt-2" />
+    </Card>
+    <Card className="max-w-xs mx-auto">
+      <Text>Revenue</Text>
+      <Metric>$ 71,465</Metric>
+      <Flex className="mt-4">
+        <Text>32% of annual target</Text>
+        <Text>$ 225,000</Text>
+      </Flex>
+      <ProgressBar value={32} className="mt-2" />
+    </Card>
+    </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
